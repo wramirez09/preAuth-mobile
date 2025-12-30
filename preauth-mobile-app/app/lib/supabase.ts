740a0363fc386c3e28d51c5426efeee3c0f9e942
Will Ramirez <wramirez1980@gmail.com>
2025-12-29 20:51:03 -0600

    chore: remove keys

--- preauth-mobile-app/app/lib/supabase.ts
+++ preauth-mobile-app/app/lib/supabase.ts
@@ -1,8 +1,8 @@
 import { AppState, Platform } from 'react-native'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { createClient, processLock } from '@supabase/supabase-js'
-const supabaseUrl = "https://ewehlzmdgrdiejtifamp.supabase.co"
-const supabaseAnonKey = "=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV3ZWhsem1kZ3JkaWVqdGlmYW1wIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0NzUzNzk4MiwiZXhwIjoyMDYzMTEzOTgyfQ.foXdsGP_5wRE1evIGHElL1b2e372377vmfYhzUO54Qw"
+const supabaseUrl = ""
+const supabaseAnonKey = ""
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey, {
     auth: {
