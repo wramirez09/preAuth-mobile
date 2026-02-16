98c0bf80569c40c25bec3c37dbf84ef48d89bd4d
Will Ramirez <wramirez1980@gmail.com>
2026-02-15 21:12:56 -0600

    chore: build updates for deployment

--- metro.config.js
+++ metro.config.js
@@ -1,9 +1,9 @@
-const { getDefaultConfig } = require('expo/metro-config');
-const { withNativeWind } = require('nativewind/metro');
+const { getDefaultConfig } = require('expo/metro-config')
+const { withNativewind } = require('nativewind/metro')
 
-const config = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname)
 
-module.exports = withNativeWind(config, {
+module.exports = withNativewind(config, {
   input: './global.css',
   configPath: './tailwind.config.js',
-});
+})
